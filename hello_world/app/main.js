923aa8706d0dc78c573ebab67f2feb3f518c729b
Satish Kumar Pokala <s.pokala@everteam.com>
2016-08-09 17:07:44 +0530

    hello world example

---
+++ hello_world/app/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by satish on 09/08/16.
+ */
+"use strict";
+const platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+const app_component_1 = require('./app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
---
+++ hello_world/app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by satish on 09/08/16.
+ */
+
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);

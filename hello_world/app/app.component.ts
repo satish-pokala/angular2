7baf20e00154c65931cefd41754376dc47a16e96
Satish Kumar Pokala <s.pokala@everteam.com>
2016-08-11 11:39:48 +0530

    Updated with RC 5 version

--- hello_world/app/app.component.ts
+++ hello_world/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>hello world</h1>'
+    template: '<h1>My First Angular 2 Hello World App!!!</h1>'
 })
-export class AppComponent {}
+
+export class AppComponent{ }
---
+++ hello_world/app/app.module.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by satish on 11/08/16.
+ */
+
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+})
+
+export class AppModule { }
